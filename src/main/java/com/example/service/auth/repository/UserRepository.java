fe93617a7d65c0a82392dca97b6077f40e0ed7c0
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-08-06 20:22:15 -0300

    User repository class and entity representation

---
+++ src/main/java/com/example/service/auth/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.service.auth.repository;
+
+import com.example.service.auth.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    UserDetails findByLoginAndActiveTrue(String username);
+
+
+    boolean existsByLogin(String login);
+}
