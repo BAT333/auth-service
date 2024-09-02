e25427346080c94c30f08988dd2d77ce9d1131af
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-09-02 18:37:10 -0300

    SpringDoc Api Documentation

--- src/main/java/com/example/service/auth/config/SpringDoc/SpringDocConfig.java
+++ src/main/java/com/example/service/auth/config/SpringDoc/SpringDocConfig.java
@@ -2,7 +2,10 @@ package com.example.service.auth.config.SpringDoc;
 
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,14 @@ public class SpringDocConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(
+                   new Info().title("Auth service api")
+                           .description("THIS PART OF THE API VALIDATES WHO CAN ACCESS AND WHAT THEY CAN ACCESS")
+                           .version("v0.0.1")
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .description("")
+                    .url(""))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
--- src/main/java/com/example/service/auth/controller/UserController.java
+++ src/main/java/com/example/service/auth/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.service.auth.controller;
 import com.example.service.auth.model.DataLoginDTO;
 import com.example.service.auth.model.DataToken;
 import com.example.service.auth.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +15,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @SecurityRequirement(name = "bearer-key")
 public class UserController {
     @Autowired
     private UserService service;
     @PostMapping
     @Transactional
+    @Operation(summary ="User login", description = "User enters their registered credentials and receives a token back allowing access to the necessary information.")
+
     public ResponseEntity<DataToken> login(@RequestBody @Valid DataLoginDTO dto){
         return ResponseEntity.ok(service.login(dto));
     }
     @PostMapping("/register")
     @Transactional
+    @Operation(summary ="Registers new users", description = "Register a user with the necessary information and send it for validation")
     public ResponseEntity<DataLoginDTO> register(@RequestBody @Valid DataLoginDTO dto){
         return ResponseEntity.ok(service.register(dto));
     }
